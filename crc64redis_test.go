83c8c0174a867dd71839ae532cf446b07b8bf5d8
Andrey Smirnov <Smirnov.Andrey@gmail.com>
2013-11-19 15:10:18 +0400

    Benchmark for crc64.

--- crc64redis_test.go
+++ crc64redis_test.go
@@ -10,3 +10,11 @@ func TestRedisCRC64(t *testing.T) {
 		t.Errorf("crc64 doesn't match: crc64(\"123456789\") = %#v != 0xe9c6d914c4b8d9ca", hash)
 	}
 }
+
+func BenchmarkRedisCRC64(b *testing.B) {
+	data := []byte{'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+	var crc uint64
+	for i := 0; i < b.N; i++ {
+		CRC64Update(crc, data)
+	}
+}
